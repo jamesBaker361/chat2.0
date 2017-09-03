a71d2e7d73be791c488ccb74e49976b9d2a497d9
jamesBaker361 <jlbaker361@gmail.com>
2017-09-03 15:59:51 -0400

    ugh

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.use(parser.urlencoded({     // to support URL-encoded bodies
 		});
 
 		socket.on("registerReq",function(data){
-			var y={okay:true};
+
 			db.collection("users").find({name:data.name},{}).toArray(function(err,out){
 				//console.log(out);
 				if(out.length>0){
@@ -121,7 +121,7 @@ app.use(parser.urlencoded({     // to support URL-encoded bodies
 				}else{
 					friend.newUser(db,data.name,data.password,function(u){
 						friend.targetClient(socket,clients,function(boi){
-							boi.emit("registerRes",y);
+							boi.emit("registerRes",{okay:true});
 						})
 					})
 				}
